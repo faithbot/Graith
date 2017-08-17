79053ed8e5b029d1f13e21f3d0f9c17fcd84dfa9
Faith Schneider <niahcx@yahoo.com>
2017-08-17 15:49:27 -0600

    added gulp requires

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var themename = 'Graith';
+
+var gulp = require('gulp'),
+	browserSync = require('browser-sync').create(),
+	image = require('gulp-image'),
+	jshint = require('gulp-jshint'),
+	less = require('gulp-less'),
+	uglify = require('gulp-uglify'),
+	plumber = require('gulp-plumber'),
+	sourcemaps = require('gulp-sourcemaps')
+;
