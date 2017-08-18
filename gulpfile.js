a0cb0d4bff05d13269f39d7f8b243eef6e1f74ba
Faith Schneider <niahcx@yahoo.com>
2017-08-17 19:23:21 -0600

    configured gulpfile to complie less

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,3 @@
-var themename = 'Graith';
-
 var gulp = require('gulp'),
 	browserSync = require('browser-sync').create(),
 	image = require('gulp-image'),
@@ -9,3 +7,48 @@ var gulp = require('gulp'),
 	plumber = require('gulp-plumber'),
 	sourcemaps = require('gulp-sourcemaps')
 ;
+
+
+// variables
+
+// app theme folder
+// /wedding/wp-content/themes/graith/
+destination = '/wedding/wp-content/themes/graith/',
+
+scss = '/dev/sass/',
+js = '/dev/js/',
+img = '/dev/img/',
+// languages ='/dev/languages/';
+
+
+// tasks
+
+// Uglify/minify scripts
+gulp.task('scripts', function(){
+	gulp.src('js/**/*.js')
+	.pipe(plumber())
+	//.pipe(concat('scripts.js'))
+	.pipe(uglify())
+	.pipe(gulp.dest('/wedding/wp-content/themes/graith/'));
+});
+
+// Compile Less
+gulp.task('styles', function() {
+	gulp.src('dev/less/styles.less')
+	.pipe(plumber())
+	.pipe(less({
+		filename: 'styles.less',
+		compress: true
+	}))
+	.pipe(gulp.dest('wedding/wp-content/themes/graith/'));
+});
+
+
+// Watch file changes
+gulp.task('watch', function() {
+	gulp.watch('dev/js/**/*.js', ['scripts']);
+	gulp.watch('dev/less/**/*.less', ['styles']);
+});
+
+// Default task
+gulp.task('default', ['scripts', 'styles', 'watch']);
