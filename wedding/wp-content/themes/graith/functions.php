6ca055d4182a76a7e562ae0026bdd91c1483ec1a
Faith Schneider <niahcx@yahoo.com>
2017-09-19 12:03:54 -0600

    Upload image, add home page template

--- wedding/wp-content/themes/graith/functions.php
+++ wedding/wp-content/themes/graith/functions.php
@@ -120,7 +120,9 @@ function graith_scripts() {
 	wp_enqueue_style( 'graith-style', get_stylesheet_uri() );
 	wp_enqueue_style ('theme-style', get_template_directory_uri().'/styles.css');
 
+	/* mobile menu
 	wp_enqueue_script( 'graith-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	*/
 
 	wp_enqueue_script( 'graith-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
