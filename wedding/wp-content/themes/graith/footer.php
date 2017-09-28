abca82d574fe8aad329eca67eb61fd419789d6f4
Faith Schneider <niahcx@yahoo.com>
2017-09-27 19:47:39 -0600

    adding css

--- wedding/wp-content/themes/graith/footer.php
+++ wedding/wp-content/themes/graith/footer.php
@@ -13,19 +13,12 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer">
+	<footer class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'graith' ) ); ?>"><?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'graith' ), 'WordPress' );
-			?></a>
-			<span class="sep"> | </span>
-			<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'graith' ), 'graith', '<a href="http://underscores.me/">Underscores.me</a>' );
-			?>
+			<p>Made by Faithbot</p>
 		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+	</footer>
+
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
