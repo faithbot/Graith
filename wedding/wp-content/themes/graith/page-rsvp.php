a2637742f8d8e4d3ce3c79563caa94dd7d6a6295
Faith Schneider <niahcx@yahoo.com>
2018-01-04 00:08:57 -0700

    try and make RSVP form work

--- wedding/wp-content/themes/graith/page-rsvp.php
+++ wedding/wp-content/themes/graith/page-rsvp.php
@@ -7,6 +7,57 @@
  * @package Graith
  */
 
+  $response = "";
+//function to generate response
+	function my_contact_form_generate_response($type, $message){
+
+		global $response;
+
+		if($type == "success") $response = "<div class='success'>{$message}</div>";
+		else $response = "<div class='error'>{$message}</div>";
+
+	}
+  //response messages
+  $message_unsent  = "Message was not sent. Try Again.";
+  $message_sent    = "Thanks! Your message has been sent.";
+
+
+//user posted variables
+if (isset($_POST['full-name'])) {
+	$name = $_POST['full-name'];
+}
+if (isset($_POST['email'])) {
+	$email = $_POST['email'];
+}
+if (isset($_POST['other-names'])) {
+	$otherNames = $_POST['other-names'];
+}
+if (isset($_POST['attend'])) {
+	$attend = $_POST['attend'];
+}
+
+
+//php mailer variables
+//$to = get_option('admin_email');
+$to = 'faith@faithbot.com';
+$subject = "Someone sent a message from ".get_bloginfo('name');
+$headers = 'From: '. $email . "\r\n" .
+  'Reply-To: ' . $email . "\r\n";
+
+// on form submit
+if (isset($_POST['submit'])) 
+{ 
+    $sent = wp_mail($to, $subject, strip_tags($message), $headers);
+		if($sent) my_contact_form_generate_response("success", $message_sent); //message sent!
+		else my_contact_form_generate_response("error", $message_unsent); //message wasn't sent
+}
+else
+{
+    // Show Form
+}
+
+
+
 get_header(); ?>
 
 	<div id="primary" class="content-area">
@@ -19,10 +70,13 @@ get_header(); ?>
 
 			endwhile; // End of the loop.
 			?>
-
+			
+			
+			<?php echo $response; ?>
+			
 			
 			<!-- RSVP form -->
-			<form>
+			<form action="<?php the_permalink(); ?>" method="post">
 				
 				<div class="form-group">
 					<label for="full-name">Full Name <span class="required">*</span></label>
@@ -36,20 +90,19 @@ get_header(); ?>
 				
 				<div class="form-group">
 					<label for="other-names">Additional Guest Names</label>
-					<textarea name="email" class="form-control" rows="4">
-					</textarea>
+					<textarea name="other-names" name="other-names" class="form-control" rows="4"></textarea>
 				</div>
 				
 				<div class="form-group">
 				
 					<div class="form-check">
-						<input class="form-check-input" type="radio" name="can-attend" id="can-attend" value="option1" checked>
+						<input class="form-check-input" type="radio" name="attend" id="can-attend" value="can-attend" checked>
 						<label class="form-check-label" for="can-attend">
 							Will Attend
 						</label>
 					</div>
 					<div class="form-check">
-						<input class="form-check-input" type="radio" name="cant-attend" value="cant-attend">
+						<input class="form-check-input" type="radio" name="attend" id="cant-attend" value="cant-attend">
 						<label class="form-check-label" for="cant-attend">
 							Regretfully Decline
 						</label>
@@ -57,7 +110,7 @@ get_header(); ?>
 					
 				</div>
 
-				
+				<input type="hidden" name="submitted" value="1">
 				<button type="submit" class="btn">Submit</button>
 				
 			</form>
